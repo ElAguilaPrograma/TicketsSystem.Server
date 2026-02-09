f00c0c58a777fd0ce45880ee4b7b46be876dd4c6
ElAguilaPrograma <je.munozm@outlook.com>
2026-02-08 21:41:31 -0600

    feat: Creating a generic repository

---
+++ TicketsSystem.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketsSystem.Domain.Interfaces;
+
+namespace TicketsSystem.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly SystemTicketsContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(SystemTicketsContext systemTicketsContext)
+        {
+            _context = systemTicketsContext;
+            _dbSet = systemTicketsContext.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAll() => await _dbSet.ToListAsync();
+        public async Task<T?> GetById(Guid id) => await _dbSet.FindAsync(id);
+
+        public async Task Create(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ TicketsSystem.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketsSystem.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T?> GetById(Guid id);
+        Task Create(T entity);
+        Task Update(T entity);
+    }
+}
