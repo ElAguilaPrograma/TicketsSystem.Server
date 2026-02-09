1cb52dbbc1e1bfde1ae0c32a33726c42f577d960
ElAguilaPrograma <je.munozm@outlook.com>
2026-02-08 21:47:21 -0600

    feat: Crate UnitOfWork to manage save changes on application layer

---
+++ TicketsSystem.Data/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using TicketsSystem.Domain.Interfaces;
+
+namespace TicketsSystem.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SystemTicketsContext _context;
+
+        public UnitOfWork(SystemTicketsContext systemTicketsContext)
+        {
+            _context = systemTicketsContext;
+        }
+
+        public Task<int> SaveChangesAsync() => _context.SaveChangesAsync();
+    }
+}
---
+++ TicketsSystem.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace TicketsSystem.Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync();
+    }
+}
